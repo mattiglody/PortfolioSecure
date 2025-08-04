3279cce70bc221ae5cb95fbadcab82bea507e5a0
mattiglo91 <mattiglody@outlook.com>
2025-08-04 18:07:12 -0400

    features new

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ const commandDescriptions = {
 
 const bioParts = [
   "Welcome to my professional digital footprint.\n" +
+  "  \n" +
   "I'm glad you could take the time to stop by and check it out.\n" +
   "My name is Matthew Iglody,\n" +
   "I am a mid-career professional who has been working in IT since College.\n" +
@@ -63,5 +64,9 @@ const bioParts = [
   "hit NYC, over the next two years we worked to maintain and update our colleauges tele presence and\n" +
   "around spring of 2022 NYC felt like it was getting it's traffic back. There were more people coming\n" +
   "back onsite and policies were changing.",
-  "In December of 2022 I got the opportunity to make the switch into Research IT, Clinical trials, and..."
+  "In December of 2022 I got the opportunity to make the switch into Research IT, Clinical trials, and...\n" +
+  "I was there for another two years",
+  "Congrats! You've reached the end of my bio.\n" +
+  "You have unlocked the code for a non-cartoon headshot!\n" +
+  "It is pic-a or p-a | Enjoy, and thanks again!",
 ];
